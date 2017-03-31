9a76c7508fe8c06175922601c1c344ca45ea9be4
Carsten König <Carsten@gettingsharper.de>
2017-03-31 10:45:32 +0200

    can move block with arrow keys

--- src/Main.purs
+++ src/Main.purs
@@ -2,11 +2,20 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Timer (IntervalId, TIMER, setInterval)
+import Control.Monad.Except (runExcept)
 import Control.Monad.ST (modifySTRef, readSTRef, newSTRef, STRef, ST)
+import DOM (DOM)
+import DOM.Event.Event (Event)
+import DOM.Event.EventTarget (eventListener, addEventListener)
+import DOM.Event.KeyboardEvent (code, eventToKeyboardEvent)
+import DOM.Event.Types (EventType(EventType))
+import DOM.HTML (window)
+import DOM.HTML.Types (windowToEventTarget)
+import Data.Either (Either(Right))
 import Data.Int (toNumber)
 import Data.Maybe (Maybe(Just))
-import Data.Unit (Unit)
 import Graphics.Canvas (fillRect, Context2D, CanvasElement, ScaleTransform, CANVAS, scale, rect, fillPath, setFillStyle, getContext2D, getCanvasElementById, getCanvasHeight, getCanvasWidth)
 import Partial.Unsafe (unsafePartial)
 
@@ -63,6 +72,39 @@ loop ctx game = do
 drop :: FallingBlock -> FallingBlock
 drop block = block { posY = block.posY + 1 }  
 
+moveRight :: FallingBlock -> FallingBlock
+moveRight block = block { posX = block.posX + 1 }
+
+moveLeft :: FallingBlock -> FallingBlock
+moveLeft block = block { posX = block.posX - 1 }
+
+
+initializeInput :: forall s e. Context2D -> GameState s -> Eff ( dom :: DOM, st :: ST s, canvas :: CANVAS | e) Unit
+initializeInput ctx game = do
+  w <- window
+  addEventListener 
+    (EventType "keydown")
+    (eventListener $ onKeyPress ctx game)
+    false
+    (windowToEventTarget w)
+  pure unit
+
+
+onKeyPress :: forall eff s. Context2D -> GameState s -> Event -> Eff ( st :: ST s, canvas :: CANVAS| eff) Unit
+onKeyPress ctx game event = do
+  case map code (runExcept (eventToKeyboardEvent event)) of
+    Right "ArrowLeft" -> do
+      modifySTRef game.fallingBlock moveLeft
+      drawGame ctx game
+    Right "ArrowRight" -> do
+      modifySTRef game.fallingBlock moveRight
+      drawGame ctx game
+    Right "ArrowDown" -> do
+      modifySTRef game.fallingBlock drop
+      drawGame ctx game
+    _ -> 
+      pure unit
+
 
 -- calculates a ScaleTransform so we can use 1pixel per block
 calculateScaling :: forall e. Int -> Int -> CanvasElement          
@@ -73,16 +115,16 @@ calculateScaling w h canvas = do
   pure { scaleX: cW / (toNumber w), scaleY: cH / (toNumber h) }
 
 
-main :: forall e s. Eff ( canvas :: CANVAS, st :: ST s, timer :: TIMER | e) Unit
+main :: forall e s. Eff ( canvas :: CANVAS, st :: ST s, timer :: TIMER, dom :: DOM , console :: CONSOLE| e) Unit
 main = void $ unsafePartial do
     game <- initializeGame
-
     Just canvas <- getCanvasElementById "canvas"
     ctx <- getContext2D canvas
 
     scaling <- calculateScaling 12 20 canvas
     scale scaling ctx
 
+    initializeInput ctx game
     loopId <- initializeLoop ctx game
     drawGame ctx game
 
