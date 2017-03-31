4f6e7ae73f17f04baad8e9f50dc3bf232b570663
Carsten König <Carsten@gettingsharper.de>
2017-03-31 09:11:07 +0200

    block falls down

--- src/Main.purs
+++ src/Main.purs
@@ -2,11 +2,12 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.ST (readSTRef, newSTRef, STRef, ST)
+import Control.Monad.Eff.Timer (IntervalId, TIMER, setInterval)
+import Control.Monad.ST (modifySTRef, readSTRef, newSTRef, STRef, ST)
 import Data.Int (toNumber)
 import Data.Maybe (Maybe(Just))
 import Data.Unit (Unit)
-import Graphics.Canvas (Context2D, CanvasElement, ScaleTransform, CANVAS, scale, rect, fillPath, setFillStyle, getContext2D, getCanvasElementById, getCanvasHeight, getCanvasWidth)
+import Graphics.Canvas (fillRect, Context2D, CanvasElement, ScaleTransform, CANVAS, scale, rect, fillPath, setFillStyle, getContext2D, getCanvasElementById, getCanvasHeight, getCanvasWidth)
 import Partial.Unsafe (unsafePartial)
 
 
@@ -29,6 +30,9 @@ initializeGame = do
 
 drawGame :: forall e s. Context2D -> GameState s -> Eff (canvas :: CANVAS, st :: ST s | e) Unit
 drawGame ctx game = do
+  setFillStyle "#000000" ctx
+  fillRect ctx { x: 0.0, y: 0.0, w: 12.0, h: 20.0 }
+
   block <- readSTRef game.fallingBlock
   drawFalling ctx block
 
@@ -45,6 +49,21 @@ drawFalling ctx block = do
     pure unit
   
 
+initializeLoop :: forall e s. Context2D -> GameState s -> Eff (timer :: TIMER, st :: ST s, canvas :: CANVAS | e) IntervalId
+initializeLoop ctx game = do
+  setInterval 1000 (loop ctx game)
+
+
+loop :: forall e s. Context2D -> GameState s -> Eff (st :: ST s, canvas :: CANVAS | e) Unit
+loop ctx game = do
+  modifySTRef game.fallingBlock drop
+  drawGame ctx game
+
+
+drop :: FallingBlock -> FallingBlock
+drop block = block { posY = block.posY + 1 }  
+
+
 -- calculates a ScaleTransform so we can use 1pixel per block
 calculateScaling :: forall e. Int -> Int -> CanvasElement          
         -> Eff ( canvas :: CANVAS | e) ScaleTransform               
@@ -54,7 +73,7 @@ calculateScaling w h canvas = do
   pure { scaleX: cW / (toNumber w), scaleY: cH / (toNumber h) }
 
 
-main :: forall e s. Eff ( canvas :: CANVAS, st :: ST s | e) Unit
+main :: forall e s. Eff ( canvas :: CANVAS, st :: ST s, timer :: TIMER | e) Unit
 main = void $ unsafePartial do
     game <- initializeGame
 
@@ -64,4 +83,6 @@ main = void $ unsafePartial do
     scaling <- calculateScaling 12 20 canvas
     scale scaling ctx
 
+    loopId <- initializeLoop ctx game
     drawGame ctx game
+
