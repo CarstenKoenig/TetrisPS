ec044158271e374a22ba58c1f5c51c927d9143b5
Carsten König <carsten@gettingsharper.de>
2017-04-04 21:31:18 +0200

    refactored Game module into pure data/functions

--- src/Game.purs
+++ src/Game.purs
@@ -2,6 +2,7 @@ module Game
     ( GameState
     , GameSettings
     , FallingTetromino 
+    , Color
     , initializeGame
     , updateMovingBlock
     , updateFalling
@@ -15,7 +16,6 @@ where
 import Prelude
 import Data.Map as Map
 import Control.Monad.Eff (Eff)
-import Control.Monad.ST (ST, STRef, newSTRef, readSTRef, writeSTRef)
 import Data.Map (Map)
 import Data.Traversable (all)
 import Data.Tuple (snd, Tuple(Tuple), fst)
@@ -32,38 +32,11 @@ type GameSettings =
     , cols :: Int
     }
 
-data GameState s =
-    GameState (STRef s 
-        { fallingTetromino :: FallingTetromino
-        , occupied :: Map Point Color
-        , settings :: GameSettings
-        })
-
-
-fallingTetromino :: forall s e. GameState s -> Eff ( st :: ST s | e) FallingTetromino
-fallingTetromino (GameState gs) = do
-    gamestate <- readSTRef gs
-    pure $ gamestate.fallingTetromino
-
-
-setFallingTetromino :: forall s e. GameState s -> FallingTetromino -> Eff ( st :: ST s | e) Unit
-setFallingTetromino (GameState gs) ftetr = do
-    gamestate <- readSTRef gs
-    let gamestate' = gamestate { fallingTetromino = ftetr }
-    writeSTRef gs gamestate'
-    pure unit
-
-
-occupied :: forall s e. GameState s -> Eff ( st :: ST s | e) (Map Point Color)
-occupied (GameState gs) = do
-    gamestate <- readSTRef gs
-    pure $ gamestate.occupied
-
-
-settings :: forall s e. GameState s -> Eff ( st :: ST s | e) GameSettings
-settings (GameState gs) = do
-    gamestate <- readSTRef gs
-    pure $ gamestate.settings
+type GameState =
+    { fallingTetromino :: FallingTetromino
+    , occupied :: Map Point Color
+    , settings :: GameSettings
+    }
 
 
 type FallingTetromino = 
@@ -72,58 +45,47 @@ type FallingTetromino =
   }
 
 
-initializeGame :: forall e s.                                                   
-  GameSettings -> Eff ( st :: ST s | e) (GameState s)                                                            
+initializeGame :: GameSettings -> GameState
 initializeGame sets = do
     let ftetr = { tetromino: tetrominoL, coord: point 5 (-2) }
-    state <- newSTRef { fallingTetromino: ftetr, occupied: (Map.empty :: Map Point Color), settings: sets }
-    pure $ GameState state
+    { fallingTetromino: ftetr, occupied: (Map.empty :: Map Point Color), settings: sets }
 
 
-updateMovingBlock :: forall s e . GameState s -> (FallingTetromino -> FallingTetromino) -> Eff (st :: ST s | e) Unit
-updateMovingBlock gs update = do
-    gamestate <- gameState gs
-    ftetr <- update' gamestate.fallingTetromino
-    setFallingTetromino gs ftetr  
+updateMovingBlock :: (FallingTetromino -> FallingTetromino) -> GameState -> GameState
+updateMovingBlock update gamestate =
+    let ftetr = update' gamestate.fallingTetromino
+    in gamestate { fallingTetromino = ftetr }
     where 
-        gameState (GameState gs) = readSTRef gs
-        update' ftetr = do
+        update' ftetr =
             let ftetr' = update ftetr
-            valid <- isValid gs ftetr'
-            pure $ if valid then ftetr' else ftetr
+                valid = isValid gamestate ftetr'
+            in if valid then ftetr' else ftetr
 
 
-updateFalling :: forall s e . GameState s -> Eff (st :: ST s | e) Unit
-updateFalling gs = do
-    gamestate <- gameState gs
+updateFalling :: GameState -> GameState
+updateFalling gamestate =
     let falling = gamestate.fallingTetromino
-    let dropped = drop falling
-    valid <- isValid gs dropped
-    if valid
+        dropped = drop falling
+        valid = isValid gamestate dropped
+    in if valid
         then
-            setFallingTetromino gs dropped
-        else do
-            stopFalling gs
-            initNewFalling gs
-    where
-        gameState (GameState gs) = readSTRef gs
-                
+            gamestate { fallingTetromino = dropped }
+        else
+            (stopFalling >>> initNewFalling) gamestate
             
 
-stopFalling :: forall s e . GameState s -> Eff (st :: ST s | e) Unit
-stopFalling gs = pure unit
+stopFalling :: GameState -> GameState
+stopFalling gs = gs
 
 
-initNewFalling :: forall s e . GameState s -> Eff (st :: ST s | e) Unit
-initNewFalling gs = pure unit
+initNewFalling :: GameState -> GameState
+initNewFalling gs = gs
 
 
-isValid :: forall e s. GameState s -> FallingTetromino ->  Eff (st :: ST s | e) Boolean
-isValid gs ftetr = do
-    gamestate <- gameState gs
-    pure $ all (valid gamestate.settings gamestate.occupied) (points ftetr.coord ftetr.tetromino)
+isValid :: GameState -> FallingTetromino ->  Boolean
+isValid gamestate ftetr =
+    all (valid gamestate.settings gamestate.occupied) (points ftetr.coord ftetr.tetromino)
     where 
-        gameState (GameState gs) = readSTRef gs
         valid setts map pt =
             insideBounds setts pt && not (isOccupied map pt)
 
@@ -139,11 +101,11 @@ insideBounds { rows: r, cols: c } (Tuple x y) =
     y < r    
 
 
-drawGame :: forall e s. Context2D -> GameState s -> Eff (canvas :: CANVAS, st :: ST s | e) Unit
-drawGame ctx gs = do
+drawGame :: forall e. Context2D -> GameState -> Eff (canvas :: CANVAS | e) Unit
+drawGame ctx gamestate = do
   setFillStyle "#03101A" ctx
   fillRect ctx { x: 0.0, y: 0.0, w: 12.0, h: 20.0 }
-  ftetr <- fallingTetromino gs
+  let ftetr = gamestate.fallingTetromino
   drawFalling ctx ftetr
 
 
--- src/Main.purs
+++ src/Main.purs
@@ -1,11 +1,12 @@
 module Main where
 
 import Prelude
+import Control.Bind ((=<<))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Timer (IntervalId, TIMER, setInterval)
 import Control.Monad.Except (runExcept)
-import Control.Monad.ST (ST)
+import Control.Monad.ST (ST, STRef, modifySTRef, newSTRef, readSTRef)
 import DOM (DOM)
 import DOM.Event.Event (Event)
 import DOM.Event.EventTarget (eventListener, addEventListener)
@@ -16,63 +17,77 @@ import DOM.HTML.Types (windowToEventTarget)
 import Data.Either (Either(Right))
 import Data.Int (toNumber)
 import Data.Maybe (Maybe(Just))
+import Data.Unit (Unit)
 import Game (GameState, GameSettings, moveLeft, moveRight, rotate, drawGame, initializeGame, updateMovingBlock, updateFalling)
 import Graphics.Canvas (ScaleTransform, CANVAS, CanvasElement, Context2D, getCanvasHeight, getCanvasWidth, scale, getContext2D, getCanvasElementById)
 import Partial.Unsafe (unsafePartial)
+
+type State s = STRef s GameState
+
+updateGame :: forall e s. (GameState -> GameState) -> State s -> Eff (st :: ST s | e) GameState
+updateGame update state = 
+  modifySTRef state update
+
+
+getGameState :: forall e s. State s -> Eff (st :: ST s | e) GameState 
+getGameState state = 
+  readSTRef state
 
 
 main :: forall e s. Eff ( canvas :: CANVAS, st :: ST s, timer :: TIMER, dom :: DOM , console :: CONSOLE| e) Unit
 main = void $ unsafePartial do
     let settings = { rows: 20, cols: 12}
-    game <- initializeGame settings
+    let game = initializeGame settings
+    state <- newSTRef game
     Just canvas <- getCanvasElementById "canvas"
     ctx <- getContext2D canvas
 
     scaling <- calculateScaling settings canvas
     scale scaling ctx
 
-    initializeInput ctx game
-    loopId <- initializeLoop ctx game
+    initializeInput ctx state
+    loopId <- initializeLoop ctx state
     drawGame ctx game
 
 
 
-initializeLoop :: forall e s. Context2D -> GameState s -> Eff (timer :: TIMER, st :: ST s, canvas :: CANVAS | e) IntervalId
-initializeLoop ctx game = do
-  setInterval 1000 (loop ctx game)
+initializeLoop :: forall e s. Context2D -> State s -> Eff (timer :: TIMER, st :: ST s, canvas :: CANVAS | e) IntervalId
+initializeLoop ctx state = do
+  setInterval 1000 (loop ctx state)
 
 
-loop :: forall e s. Context2D -> GameState s -> Eff (st :: ST s, canvas :: CANVAS | e) Unit
-loop ctx game = do
-  updateFalling game
+loop :: forall e s. Context2D -> State s -> Eff (st :: ST s, canvas :: CANVAS | e) Unit
+loop ctx state = do
+  game <- updateGame updateFalling state
   drawGame ctx game
 
 
-initializeInput :: forall s e. Context2D -> GameState s -> Eff ( dom :: DOM, st :: ST s, canvas :: CANVAS | e) Unit
-initializeInput ctx game = do
+initializeInput :: forall s e. Context2D -> State s -> Eff ( dom :: DOM, st :: ST s, canvas :: CANVAS | e) Unit
+initializeInput ctx state = do
   w <- window
   addEventListener 
     (EventType "keydown")
-    (eventListener $ onKeyPress ctx game)
+    (eventListener $ onKeyPress ctx state)
     false
     (windowToEventTarget w)
   pure unit
 
 
-onKeyPress :: forall eff s. Context2D -> GameState s -> Event -> Eff ( st :: ST s, canvas :: CANVAS| eff) Unit
-onKeyPress ctx game event = do
+onKeyPress :: forall eff s. Context2D -> State s -> Event -> Eff ( st :: ST s, canvas :: CANVAS| eff) Unit
+onKeyPress ctx state event = do
+
   case map code (runExcept (eventToKeyboardEvent event)) of
     Right "ArrowLeft" -> do
-      updateMovingBlock game moveLeft
+      game <- updateGame (updateMovingBlock moveLeft) state
       drawGame ctx game
     Right "ArrowRight" -> do
-      updateMovingBlock game moveRight
+      game <- updateGame (updateMovingBlock moveRight) state
       drawGame ctx game
     Right "ArrowDown" -> do
-      updateFalling game
+      game <- updateGame updateFalling state
       drawGame ctx game
     Right "ArrowUp" -> do
-      updateMovingBlock game rotate
+      game <- updateGame (updateMovingBlock rotate) state
       drawGame ctx game
     _ -> 
       pure unit
